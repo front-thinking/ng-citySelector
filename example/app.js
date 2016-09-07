f0620598de5be0f48b8bbaa07dd61b40e33c2c1b
front-thinking <269354528@qq.com>
2016-09-07 14:56:42 +0800

    add QRCode

--- example/app.js
+++ example/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('myApp', ['ngCitySelector']);
 app.controller('myCtrl', function($scope){
+
+    $scope.title = '所有国家或地区';
+
+    $scope.historyData = ['美国', '日本', '新加坡'];
+
     $scope.data = [
         {
             name: '中国',
@@ -61,11 +66,91 @@ app.controller('myCtrl', function($scope){
             price: '9.9',
             en: 'en',
             py: 'xibanya'
+        }, {
+            name: '巴拿马',
+            price: '9.9',
+            en: 'en',
+            py: 'banama'
+        }, {
+            name: '察哈尔',
+            price: '9.9',
+            en: 'en',
+            py: 'chahaer'
+        }, {
+            name: '德国',
+            price: '9.9',
+            en: 'en',
+            py: 'deguo'
+        }, {
+            name: '鄂尔多斯',
+            price: '9.9',
+            en: 'en',
+            py: 'eerduosi'
+        }, {
+            name: '法国',
+            price: '9.9',
+            en: 'en',
+            py: 'faguo'
+        }, {
+            name: '哥伦比亚',
+            price: '9.9',
+            en: 'en',
+            py: 'gelunbiya'
+        }, {
+            name: '加拿大',
+            price: '9.9',
+            en: 'en',
+            py: 'jianada'
+        }, {
+            name: '开罗',
+            price: '9.9',
+            en: 'en',
+            py: 'kailuo'
+        }, {
+            name: '利比亚',
+            price: '9.9',
+            en: 'en',
+            py: 'libiya'
+        }, {
+            name: '欧洲',
+            price: '9.9',
+            en: 'en',
+            py: 'ouzhou'
+        }, {
+            name: '抛物线',
+            price: '9.9',
+            en: 'en',
+            py: 'paowuxian'
+        }, {
+            name: '去哪儿',
+            price: '9.9',
+            en: 'en',
+            py: 'qunaer'
+        }, {
+            name: '思念',
+            price: '9.9',
+            en: 'en',
+            py: 'sinian'
+        }, {
+            name: '台湾',
+            price: '9.9',
+            en: 'en',
+            py: 'taiwan'
+        }, {
+            name: '王国',
+            price: '9.9',
+            en: 'en',
+            py: 'wangguo'
         }
     ];
+
+    $scope.contactSelect = {};
 
     $scope.select = function(v){
         console.log(v);
     };
+
+
+
 
 });
--- example/ng-citySelector.js
+++ example/ng-citySelector.js
@@ -8,7 +8,7 @@
  */
 ;(function (CITYSELECTOR_DIRECTIVE){
 
-    var m = angular.module('ngCitySelector', []);
+    var m = angular.module('ngCitySelector', ['ngTouch']);
     m.version = '1.0.0';
 
 
@@ -29,29 +29,19 @@
             }
         });
         // 将contactMap转换成二维数组并按字母排序。
-        if(Object.keys(addressMap).length !== 0){
+        if(Object.keys(addressMap).length !== 0){// 判断对象不为空
+            // 将对象转换为二维数组并排序
             var addressGroupArray = Object.keys(addressMap).map(function (key) {return addressMap[key]}).sort(function (a,b) {
-                if(a[0]['py'][0] >= b[0]['py'][0]){
-                    return 1;
-                } else if (a[0]['py'][0] == b[0]['py'][0]) {
-                    return 0;
-                } else {
-                    return -1;
-                }
-
+                return a[0]['py'].charCodeAt(0) - b[0]['py'].charCodeAt(0);
             });
         }
         return addressGroupArray;
     }
 
-    function getCache(){
-
-    }
-
     // 获取数据,暂时不支持缓存
     function getData(csData){
         if(!csData){
-            throw 'ng-cs-data must be unnull';
+            throw 'ng-cs-data must is empty';
         }
         if(csData !== null && typeof csData === 'object'){
             return csData;
@@ -87,18 +77,8 @@
         }
     }
 
-    // 获取顶部搜索和历史记录div的高度
-    function outerHeight(cls) {
-        var el = document.querySelectorAll('.'+cls)[0];
-        var height = el.offsetHeight;
-        var style = getComputedStyle(el);
-        height+= parseInt(style.marginTop) + parseInt(style.marginBottom);
-        return height;
-    }
-
-
     // directive定义
-    m.directive('ngCitySelector', ['$parse', '$compile', '$location', '$anchorScroll', function($parse, $compile, $location, $anchorScroll) {
+    m.directive('ngCitySelector', [function() {
         return {
             restrict: "AE",
             replace: true,
@@ -107,7 +87,47 @@
                 ngCsData: "=",
                 ngCsFn: "="
             },
-            templateUrl: 'citySelector.html',
+            template: '<div class="container country country__show-recent cityselector">\
+                                <div class="container-hd">\
+                                        <!-- S 搜索 -->\
+                                    <div class="country-search country-search__active">\
+                                    <div class="country-search_txt">\
+                                    <i class="icon-img icon-img-search02"></i>\
+                                    <span class="txt">搜索</span>\
+                                    </div>\
+                                    <input class="country-search-ipt" type="text" ng-model="search" id="search">\
+                                    </div>\
+                                        <!-- E 搜索 -->\
+                                        <!-- S 最近搜索 -->\
+                                    <div class="country-recent">\
+                                    <div class="country-recent-title">最近搜索</div>\
+                                    <ul class="country-recent-list">\
+                                    <li class="list-item" ng-repeat="h in hisSearch" ng-click="selectHis(h)">{{h}}</li>\
+                            </ul>\
+                            </div>\
+                                <!-- E 最近搜索 -->\
+                            </div>\
+                            <div class="container-bd">\
+                                    <!-- S 选择城市 -->\
+                                <div class="cityselector">\
+                                <div class="city-wrap">\
+                                <p class="city-title" ng-repeat-start="i in csData | filter: search" ng-attr-id="{{\'group-\' + i[0][\'py\'][0]}}">{{i[0].py[0] | uppercase}}</p>\
+                        <ul class="city-list" ng-repeat-end>\
+                        <li class="city-list-item" ng-repeat="j in i | filter: search" ng-click="selectItem(j)">\
+                            <span ng-bind="j.name"></span>\
+                            </li>\
+                            </ul>\
+                            </div>\
+                            <ul class="anchor-list">\
+                            <li ng-repeat="h in csData" ng-touchmove="touchMoveFn($event)" ng-touchstart="touchStartFn($event)" ng-touchend="touchEndFn($event)">\
+                            <a href="javascript:;">{{h[0].py[0] | uppercase}}</a>\
+                        </li>\
+                        </ul>\
+                        </div>\
+                            <!-- E 选择城市 -->\
+                        </div>\
+                        <div class="item-tip" ng-show="isShowTip">A</div>\
+                     </div>',
             link: function ($scope, $element, $attrs){
                 var csData = $scope.ngCsData;
                 var data = getData(csData);
@@ -115,51 +135,74 @@
                 $scope.csData = process(data);
                 $scope.search = '';
 
+
+                // 导航窗tip控制
+                var tipEle = document.querySelectorAll('.item-tip')[0];
+
                 $scope.isShowTip = false;// 导航窗tip显示控制
-                $scope.go = function (v){
-                    // 导航窗tip控制
-                    var tipEle = document.querySelectorAll('.item-tip')[0];
-                    tipEle.innerHTML = v.toUpperCase();
 
-                    $scope.isShowTip = true;
-                    setTimeout(function(){
-                        $scope.isShowTip = false;
-                        $scope.$apply();
-                    }, 500);
-
-                    // 根据导航窗跳转到对应的分类里
-                    var idStr = 'group-' + v;
-                    var scrollDis = document.getElementById(idStr).getBoundingClientRect().top - outerHeight('container-hd');
-                    window.scrollBy(0, scrollDis);
+                var scroll2Pos  = function (v){
+                    var idStr = 'group-' + v.toLowerCase();
+                    var ctlDom = document.getElementById(idStr);// 具体分类的目的dom元素
+                    if(ctlDom){ //仅当分类存在的时候滚动到对应位置，否则不滚动。（由于输入搜索关键字后，可能导航对应的分类并不存在）
+                        var scrollDis = ctlDom.getBoundingClientRect().top - 97;
+                        window.scrollBy(0, scrollDis);
+                    }
                 };
 
                 // 选中历史搜索中的某条
                 $scope.selectHis = function (v){
                     $scope.search = v;
-                };
 
-                // 添加历史记录，仅当回车键的时候添加
-                $scope.addHis = function ($evt){
-                    if($evt.which === 13){
-                        var evtValue = $evt.target.value;
-                        evtValue ? setHisSearch(evtValue) : '';//空字符串不作处理
+                    // 如果当前集合中有该条目，直接执行传进来的方法
+                    var item = data.filter(function(i){
+                        return i.name === v;
+                    });
+                    if(item){
+                        $scope.ngCsFn(item[0]);
                     }
                 };
 
                 // 选择某个国家或地区
                 $scope.selectItem = function (v){
+                    var serchStr = angular.element(document.getElementById('search')).val(); // 获取search中的值
+                    if(serchStr && (v.name.indexOf(serchStr) !== -1 || v.py.indexOf(serchStr) !== -1)){
+                        setHisSearch(v.name);
+                    }
                     $scope.ngCsFn(v);
                 };
 
+                $scope.touchStartFn = function($evt){
+                    var tip = $evt.target.innerHTML;
+                    // 增加active的class
+                    angular.element($evt.target).addClass('active');
+                    if(tipEle.innerHTML !== tip){
+                        $scope.isShowTip = true;
+                        tipEle.innerHTML = tip;
+                        scroll2Pos(tip);
+                    }
+                    $evt.preventDefault();
+
+                };
+                $scope.touchMoveFn = function($evt){
+                    var myLocation = $evt.changedTouches[0], cate = '',
+                        current = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+                    var anchorListDom = angular.element(current).parent().parent();
+                    if(anchorListDom.hasClass('anchor-list')){
+                        var tip = current.innerHTML;
+                        if(tipEle.innerHTML !== tip){
+                            tipEle.innerHTML = tip;
+                            scroll2Pos(tip);
+                        }
+                    }
+                    $evt.preventDefault();
+                };
+                $scope.touchEndFn = function($evt){
+                    angular.element($evt.target).removeClass('active');
+                    $scope.isShowTip = false;
+                    $evt.preventDefault();
+                };
             }
         };
     }]);
-
-
-
-
-
-
-
-
 })();
